50992988287220dc9904b1606f56d01c8931f4b5
Serge Savchenko <71881555+SergeSavchenko@users.noreply.github.com>
2022-12-10 18:27:35 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,12 +1,14 @@
 --Query #1
---SELECT level, math_score
+--SELECT level, score
 --FROM student_stats
+--WHERE test_id = 'MTH'
 
 --Query #2
---SELECT level, COUNT(1)
+--SELECT level, COUNT(DISTINCT(student_id))
 --FROM student_stats
 --GROUP BY level
 
 --Query #3
---SELECT id, writing_score
+--SELECT student_id, score
 --FROM student_stats
+--WHERE test_id = 'WRT'
