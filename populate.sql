3330767b627c680b87da8f6724b6441f4da0ce41
Serge Savchenko <71881555+SergeSavchenko@users.noreply.github.com>
2022-12-10 18:27:14 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,72 +1,148 @@
 -----------------------------
 --Populate student_info table
 -----------------------------
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('0', 'female', 'group B', 'standard');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('1', 'female', 'group C', 'standard');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('2', 'female', 'group B', 'standard');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('3', 'male', 'group A', 'free/reduced');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('4', 'male', 'group C', 'standard');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('5', 'female', 'group B', 'standard');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('6', 'female', 'group B', 'standard');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('7', 'male', 'group B', 'free/reduced');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('8', 'male', 'group D', 'free/reduced');
-INSERT INTO student_info(id, gender, ethnicity, lunch_type)
-VALUES('9', 'female', 'group B', 'free/reduced');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('0', 'female', 'group B');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('1', 'female', 'group C');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('2', 'female', 'group B');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('3', 'male', 'group A');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('4', 'male', 'group C');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('5', 'female', 'group B');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('6', 'female', 'group B');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('7', 'male', 'group B');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('8', 'male', 'group D');
+INSERT INTO student_info(student_id, gender, ethnicity)
+VALUES('9', 'female', 'group B');
+
+-----------------------------
+--Populate level_name table
+-----------------------------
+INSERT INTO level_name(level_id, name)
+VALUES('MS', 'Masters degree');
+INSERT INTO level_name(level_id, name)
+VALUES('BS', 'Bachelors degree');
+INSERT INTO level_name(level_id, name)
+VALUES('AS', 'Associates degree');
+INSERT INTO level_name(level_id, name)
+VALUES('SC', 'Some college');
+INSERT INTO level_name(level_id, name)
+VALUES('HS', 'High school');
+
+-----------------------------
+--Populate test_name table
+-----------------------------
+INSERT INTO test_name(test_id, name)
+VALUES('MTH', 'Mathematics');
+INSERT INTO test_name(test_id, name)
+VALUES('RDG', 'Reading');
+INSERT INTO test_name(test_id, name)
+VALUES('WRT', 'Writing');
 
 
 -----------------------------
 --Populate student_stats table
 -----------------------------
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('0', 'bachelors degree', '72', '72', '74');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('1', 'some college', '69', '90', '88');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('2', 'masters degree', '90', '95', '93');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('3', 'associates degree', '47', '57', '44');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('4', 'some college', '76', '78', '75');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('5', 'associates degree', '71', '83', '78');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('6', 'some college', '88', '95', '92');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('7', 'some college', '40', '43', '39');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('8', 'high school', '64', '64', '67');
-INSERT INTO student_stats(id, level, math_score, reading_score, writing_score)
-VALUES('9', 'high school', '38', '60', '50');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('0', 'BS', 'MTH', '72');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('0', 'BS', 'RDG', '72');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('0', 'BS', 'WRT', '74');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('1', 'SC', 'MTH', '69');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('1', 'SC', 'RDG', '90');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('1', 'SC', 'WRT', '88');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('2', 'MS', 'MTH', '90');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('2', 'MS', 'RDG', '95');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('2', 'MS', 'WRT', '93');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('3', 'AS', 'MTH', '47');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('3', 'AS', 'RDG', '57');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('3', 'AS', 'WRT', '44');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('4', 'SC', 'MTH', '76');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('4', 'SC', 'RDG', '78');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('4', 'SC', 'WRT', '75');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('5', 'AS', 'MTH', '71');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('5', 'AS', 'RDG', '83');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('5', 'AS', 'WRT', '78');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('6', 'SC', 'MTH', '88');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('6', 'SC', 'RDG', '95');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('6', 'SC', 'WRT', '92');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('7', 'SC', 'MTH', '40');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('7', 'SC', 'RDG', '43');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('7', 'SC', 'WRT', '39');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('8', 'HS', 'MTH', '64');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('8', 'HS', 'RDG', '64');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('8', 'HS', 'WRT', '67');
+
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('9', 'HS', 'MTH', '38');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('9', 'HS', 'RDG', '60');
+INSERT INTO student_stats(student_id, level, test_id, score)
+VALUES('9', 'HS', 'WRT', '50');
+
+
+
 
 -----------------------------
---Populate education_level table
+--Populate student_level table
 -----------------------------
-INSERT INTO education_level(level, test_preparation)
-VALUES('bachelors degree', 'none');
-INSERT INTO education_level(level, test_preparation)
-VALUES('some college', 'completed');
-INSERT INTO education_level(level, test_preparation)
-VALUES('masters degree', 'none');
-INSERT INTO education_level(level, test_preparation)
-VALUES('associates degree', 'none');
-INSERT INTO education_level(level, test_preparation)
-VALUES('some college', 'none');
-INSERT INTO education_level(level, test_preparation)
-VALUES('associates degree', 'none');
-INSERT INTO education_level(level, test_preparation)
-VALUES('some college', 'completed');
-INSERT INTO education_level(level, test_preparation)
-VALUES('some college', 'none');
-INSERT INTO education_level(level, test_preparation)
-VALUES('high school', 'completed');
-INSERT INTO education_level(level, test_preparation)
-VALUES('high school', 'none');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('0', 'BS', 'none');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('1', 'SC', 'completed');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('2', 'MS', 'none');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('3', 'AS', 'none');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('4', 'SC', 'none');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('5', 'AS', 'none');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('6', 'SC', 'completed');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('7', 'SC', 'none');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('8', 'HS', 'completed');
+INSERT INTO student_level(student_id, level, test_preparation)
+VALUES('9', 'HS', 'none');
