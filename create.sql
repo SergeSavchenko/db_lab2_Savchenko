7899ecd6b1bd3fbbcd1a1d51edebae92bb94f05e
Serge Savchenko <71881555+SergeSavchenko@users.noreply.github.com>
2022-12-10 18:25:54 +0300

    Edited create.sql

--- create.sql
+++ create.sql
@@ -2,35 +2,59 @@
 
 CREATE TABLE student_info
 (
-  id          char(10)  NOT NULL ,
-  gender      char(10)  NOT NULL ,
-  ethnicity   char(20)  NOT NULL ,
-  lunch_type  char(20) NOT NULL
+  student_id  char(10)  NOT NULL,
+  gender      char(10)  NOT NULL,
+  ethnicity   char(20)  NOT NULL
 );
 
 
 CREATE TABLE student_stats
 (
-  id            char(10)  NOT NULL ,
-  level         char(25)  NOT NULL ,
-  math_score    char(10)  NOT NULL ,
-  reading_score char(10)  NOT NULL ,
-  writing_score char(10)  NOT NULL 
+  student_id  char(10)  NOT NULL,
+  level       char(25)  NOT NULL,
+  test_id     char(10)  NOT NULL,
+  score       char(10)  NOT NULL 
 );
 
-CREATE TABLE education_level
+CREATE TABLE student_level
 (
+  student_id       char(10) NOT NULL,
   level            char(25) NOT NULL,
   test_preparation char(10) NOT NULL
 );
 
---Defining primary keys
-ALTER TABLE student_info ADD CONSTRAINT PK_student_info PRIMARY KEY (id);
-ALTER TABLE student_stats ADD CONSTRAINT PK_student_stats PRIMARY KEY (level);
+CREATE TABLE level_name
+(
+  level_id char(10) NOT NULL,
+  name     char(30) NOT NULL
+);
 
+CREATE TABLE test_name
+(
+  test_id char(10) NOT NULL,
+  name    char(30) NOT NULL
+);
+
+--Defining primary keys
+ALTER TABLE student_info ADD CONSTRAINT PK_student_info PRIMARY KEY (student_id);
+
+ALTER TABLE student_stats ADD CONSTRAINT PK_student_stats PRIMARY KEY (student_id, level, test_id);
+
+ALTER TABLE student_level ADD CONSTRAINT PK_student_level PRIMARY KEY (student_id, level);
+
+ALTER TABLE level_name ADD CONSTRAINT PK_level_name PRIMARY KEY (level_id);
+
+ALTER TABLE test_name ADD CONSTRAINT PK_test_name PRIMARY KEY (test_id);
 
 --Defining foreign keys
+ALTER TABLE student_level
+ADD CONSTRAINT FK_student_level_student_info FOREIGN KEY (student_id) REFERENCES student_info (student_id);
+ALTER TABLE student_level
+ADD CONSTRAINT FK_student_level_level_name FOREIGN KEY (level) REFERENCES level_name (level_id);
+
 ALTER TABLE student_stats
-ADD CONSTRAINT FK_student_stats FOREIGN KEY (id) REFERENCES student_info (id);
-ALTER TABLE education_level
-ADD CONSTRAINT FK_education_level FOREIGN KEY (level) REFERENCES student_stats (level);
+ADD CONSTRAINT FK_student_stats_student_info FOREIGN KEY (student_id) REFERENCES student_info (student_id);
+ALTER TABLE student_stats
+ADD CONSTRAINT FK_student_stats_level_name FOREIGN KEY (level) REFERENCES level_name (level_id);
+ALTER TABLE student_stats
+ADD CONSTRAINT FK_student_stats_test_name FOREIGN KEY (test_id) REFERENCES test_name (test_id);
